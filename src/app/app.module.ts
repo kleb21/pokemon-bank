74a5ddf586c4cf8015e2f7b5dc7b60201c8e1c46
Caleb Martinez <96499418+kleb21@users.noreply.github.com>
2023-04-30 23:44:26 -0600

    second commit- some views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: ''}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
